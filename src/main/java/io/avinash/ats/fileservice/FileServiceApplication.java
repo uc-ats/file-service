a97d8904636ee20bc2ce77ede76eb56a2d95efb1
Avinash Verma <avinashverma2908@gmail.com>
2025-08-10 09:34:15 +0530

    init commit

---
+++ src/main/java/io/avinash/ats/fileservice/FileServiceApplication.java
@@ -0,0 +1,13 @@
+package io.avinash.ats.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+}
